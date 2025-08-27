3211b9c7aa2b4806efd929fe88e7c5b45db67b4a
itsRamit <ece22145@iiitkalyani.ac.in>
2025-08-27 19:49:32 +0530

    refactor: added scrapper-api in amazon scrapper

--- scrapper/amazonScrapper.js
+++ scrapper/amazonScrapper.js
@@ -1,22 +1,17 @@
 require('dotenv').config({ path: './.env' });
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { HttpsProxyAgent } = require('https-proxy-agent');
 
 async function scrapeAmazonProduct(url) {
   if (!url) return;
 
-  const host = process.env.WS_PROXY_HOST;
-  const port = process.env.WS_PROXY_PORT;
-  const username = process.env.WS_PROXY_USER;
-  const password = process.env.WS_PROXY_PASS;
-
-  const proxyUrl = `http://${username}:${password}@${host}:${port}`;
-  const agent = new HttpsProxyAgent(proxyUrl);
-
   try {
-    const response = await axios.get(url, {
-      httpsAgent: agent,
+    const apiKey = process.env.SCRAPER_API_KEY;
+    const apiUrl = `https://api.scraperapi.com?api_key=${apiKey}&url=${encodeURIComponent(
+      url
+    )}&country_code=in`;
+
+    const response = await axios.get(apiUrl, {
       headers: {
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
@@ -32,7 +27,8 @@ async function scrapeAmazonProduct(url) {
       $('.priceToPay span.a-price-whole').text().trim() ||
       $('.a.size.base.a-color-price').text().trim() ||
       $('.a-button-selected .a-color-base').text().trim() ||
-      $('.a-price .a-offscreen').text().trim() || '0';
+      $('.a-price .a-offscreen').text().trim() ||
+      '0';
 
     const outOfStock =
       $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
@@ -45,7 +41,7 @@ async function scrapeAmazonProduct(url) {
     const imageUrls = Object.keys(JSON.parse(images));
     const numericPrice = parseInt(price.replace(/[^0-9]/g, ''), 10);
 
-    const data = {
+    return {
       url,
       platform: 'amazon',
       title,
@@ -57,7 +53,6 @@ async function scrapeAmazonProduct(url) {
       highestPrice: numericPrice,
       averagePrice: numericPrice,
     };
-    return data;
   } catch (e) {
     console.error('Scraping Error:', e.message);
     throw e;
